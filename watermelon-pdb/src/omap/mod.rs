efc86e95b08acf1743ceee85c2f581cbd6485e09
Iizerd <47404005+Iizerd@users.noreply.github.com>
2025-01-16 18:57:48 -0800

    omap.

--- watermelon-pdb/src/omap/mod.rs
+++ watermelon-pdb/src/omap/mod.rs
@@ -1,1 +1,10 @@
 // Copyright (C) Back Engineering Labs, Inc. - All Rights Reserved
+
+/// Maps from new addresses to original ones in the binary. If it doesn't map
+/// to anything it is [`Self::INVALID_ADDRESS`].
+pub struct MlnOMap {
+    pub map: Vec<u32>,
+}
+impl MlnOMap {
+    pub const INVALID_ADDRESS: u32 = 0xFFFFFFFF;
+}
